ab67147bf49adeb40882bc0e04ce71c920760867
jake <palatable7@naver.com>
2022-10-27 10:21:16 +0900

    Implement

---
+++ ExMatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExMatch
+//
+//  Created by 김종권 on 2022/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExMatch/ExMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExMatchApp.swift
+//  ExMatch
+//
+//  Created by 김종권 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct ExMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
